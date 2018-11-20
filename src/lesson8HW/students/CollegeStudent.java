5e42e7916af0aca288e550a270289e188b31cf67
Valery Labunets <32488088+valerylabunets@users.noreply.github.com>
2018-11-20 12:39:32 +0200

    Added lesson8HW

--- src/lesson8HW/students/CollegeStudent.java
+++ src/lesson8HW/students/CollegeStudent.java
@@ -6,11 +6,11 @@ public class CollegeStudent extends Student {
     int rating;
     long id;
 
-    public CollegeStudent(String collegeName, int rating, long id) {
-        this.collegeName = collegeName;
-        this.rating = rating;
-        this.id = id;
-    }
+//    public CollegeStudent(String collegeName, int rating, long id) {
+//        this.collegeName = collegeName;
+//        this.rating = rating;
+//        this.id = id;
+//    }
 
     public CollegeStudent(String firstName, String lastName, int group, Course[] coursesTaken) {
         super(firstName, lastName, group, coursesTaken);
