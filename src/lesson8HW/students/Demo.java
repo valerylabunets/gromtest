1267acb16b2a7c2cbc4e1b04bd30bc1ad3a853a7
Valery Labunets <32488088+valerylabunets@users.noreply.github.com>
2018-11-20 14:44:58 +0200

    Added lesson8HW

--- src/lesson8HW/students/Demo.java
+++ src/lesson8HW/students/Demo.java
@@ -4,19 +4,19 @@ public class Demo {
 
     public static Student createHighestParent() {
 
-        Student student = new Student();
+        Student student = new Student("Ivan", "Bugrov", 10, null );
         return student;
     }
 
     public static SpecialStudent createLowestChild() {
 
-        SpecialStudent specialStudent = new SpecialStudent();
+        SpecialStudent specialStudent = new SpecialStudent(222222, "22222@gmail.com");
         return specialStudent;
     }
 
     public static void main(String[] args) {
-        System.out.println("HighestParent is " + createHighestParent().toString());
-        System.out.println("LowestChild is " + createLowestChild().toString());
+        System.out.println("Highest Parent is: " + createHighestParent().toString());
+        System.out.println("Lowest Child is: " + createLowestChild().toString());
 
     }
 }
