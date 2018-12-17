d28e4240223d3251ac32579939e5dc2dca252fac
Valery Labunets <32488088+valerylabunets@users.noreply.github.com>
2018-12-17 09:35:52 +0200

    Add lesson10HW

--- src/lesson10HW/Demo.java
+++ src/lesson10HW/Demo.java
@@ -8,14 +8,14 @@ public class Demo {
 
         //Customer customer1 = new Customer();
         ElectronicsOrder electronicsOrder1 = new ElectronicsOrder("testElectr1", new Date(), "Одесса"
-                ,"Харьков", 100, new Customer("Ivan", "Днепр", "мужской"), 12);
+                ,"Харьков", 100, new Customer("Ivan", "Днепр", "Мужской"), 12);
         ElectronicsOrder electronicsOrder2 = new ElectronicsOrder("testElectr2", new Date(), "Днепр"
-                , "Киев", 200, new Customer("Olga", "Львов", "женский"), 24);
+                , "Киев", 200, new Customer("Olga", "Львов", "Женский"), 24);
 
         FurnitureOrder furnitureOrder1 = new FurnitureOrder("testFurnit1", new Date(), "Одесса"
-                , "Львов", 500, new Customer("Ben", "Одесса", "мужской"), "111");
+                , "Львов", 500, new Customer("Ben", "Одесса", "Мужской"), "111");
         FurnitureOrder furnitureOrder2 = new FurnitureOrder("testFurnit1", new Date(), "Харьков"
-                , "Киев", 600, new Customer("Marta", "Киев", "женский"), "222");
+                , "Киев", 600, new Customer("Marta", "Киев", "Женский"), "222");
 
 
         electronicsOrder1.validateOrder();
